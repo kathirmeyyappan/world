57ed0fca96f35f0383e382ac177a82b32d5b5b3c
kathirmeyyappan <kathirmey@gmail.com>
2026-02-21 00:08:07 -0600

    serve location

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/world/',
+  base: '/', // Served at world.kathirm.com (root)
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
